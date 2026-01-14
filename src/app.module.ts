6804f3065984d6f6def8c4023f3ca905cb75f9b0
EdgarMartinezEsqueda <edgar.esqueda@outlook.com>
2026-01-14 12:10:53 -0600

    feat: Creados el controlador, mopdulo y service por defecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ExpensesController } from './expenses/expenses.controller';
+import { ExpensesService } from './expenses/expenses.service';
+import { ExpensesModule } from './expenses/expenses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ExpensesModule],
+  controllers: [AppController, ExpensesController],
+  providers: [AppService, ExpensesService],
 })
 export class AppModule {}
