66131f05d947f55b120ea6d852c04b89fd16b7fd
EdgarMartinezEsqueda <edgar.esqueda@outlook.com>
2026-01-15 23:11:53 -0600

    feat: se implementa el filtro de excepciones

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from "@nestjs/config";
-import { NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import { AllExceptionsFilter } from "./all-exceptions.filter";
 import { AppModule } from "./app.module";
 import { LoggerService } from "./logger/logger.service";
 
@@ -9,13 +10,19 @@ async function bootstrap() {
   });
   
   const configService = app.get(ConfigService);
-
+  const httpAdapterHost = app.get(HttpAdapterHost);
+  const logger = app.get(LoggerService);
+  
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapterHost, logger));
+  
   app.enableCors({
     origin: configService.getOrThrow<string>("CORS_ORIGIN"),
     methods: "GET,HEAD,PUT,POST,DELETE",
   });
-  app.useLogger(app.get(LoggerService));
+  
+  app.useLogger(logger);
   app.setGlobalPrefix("api");
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
