956652a0416c37ec6a6a746fef68c8877649d6f2
EdgarMartinezEsqueda <edgar.esqueda@outlook.com>
2026-01-16 00:19:21 -0600

    docs: agregados comentarios

--- src/expenses/dto/update-expense.dto.ts
+++ src/expenses/dto/update-expense.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateExpenseDto } from "./create-expense.dto";
 
+/**
+ * DTO para actualizar un gasto existente (todos los campos son opcionales)
+ */
 export class UpdateExpenseDto extends PartialType(CreateExpenseDto) {}
