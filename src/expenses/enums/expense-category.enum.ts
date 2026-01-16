78e0a0df8a9ffdf223033943911dde7e1cf0202f
EdgarMartinezEsqueda <edgar.esqueda@outlook.com>
2026-01-16 00:02:01 -0600

    feat: se creó un array para que funcione como enum de las posibles categorías del gasto

---
+++ src/expenses/enums/expense-category.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Enumeración de categorías de gastos permitidas
+ * Asegura tipado fuerte y validación de categorías
+ */
+export enum ExpenseCategory {
+  ALIMENTACION = "alimentacion",
+  TRANSPORTE = "transporte",
+  VIVIENDA = "vivienda",
+  ENTRETENIMIENTO = "entretenimiento",
+  SALUD = "salud",
+  EDUCACION = "educacion",
+  UTILIDADES = "utilidades",
+  OTROS = "otros",
+}
